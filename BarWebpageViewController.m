52b768d291b50654d622303656e42e4f1e3d705a
drw86312 <david.warner4@gmail.com>
2014-06-18 15:00:28 -0500

    added Yelp search

--- BarWebpageViewController.m
+++ BarWebpageViewController.m
@@ -20,16 +20,27 @@
 {
     [super viewDidLoad];
 
-    if (self.urlFromSource) {
-        NSURLRequest *request = [NSURLRequest requestWithURL:self.urlFromSource];
+    if (self.mobileURLFromSource) {
+        NSURL *mobileURL = [NSURL URLWithString:self.mobileURLFromSource];
+        NSURLRequest *request = [NSURLRequest requestWithURL:mobileURL];
         [self.webView loadRequest:request];
         self.webView.hidden = NO;
+        NSLog(@"mobileURL");
     }
-    else
+    else if (self.webURLFromSource)
     {
+        NSURL *businessURL = [NSURL URLWithString:self.webURLFromSource];
+        NSURLRequest *request = [NSURLRequest requestWithURL:businessURL];
+        [self.webView loadRequest:request];
+        self.webView.hidden = NO;
+        NSLog(@"webURL");
+
+    }
+    else {
         self.webView.hidden = YES;
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%@ appears to not have a webpage", self.placeNameFromSource] message:nil delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
         [alertView show];
+        NSLog(@"neither");
     }
 }
 
