6072326c9b7c7f53a24a71d39fba5b88f9319575
drw86312 <david.warner4@gmail.com>
2014-06-19 13:44:58 -0500

    removed email field from login and signup

--- PubChatter/ProfileViewController.m
+++ PubChatter/ProfileViewController.m
@@ -54,7 +54,9 @@
     if (![PFUser currentUser])
     {
         PFLogInViewController *loginViewController = [PFLogInViewController new];
+        loginViewController.fields = PFLogInFieldsUsernameAndPassword | PFLogInFieldsLogInButton;
         PFSignUpViewController *signupViewController = [PFSignUpViewController new];
+        signupViewController.fields = PFLogInFieldsUsernameAndPassword | PFLogInFieldsLogInButton;
         loginViewController.delegate = self;
         signupViewController.delegate = self;
         loginViewController.signUpController = signupViewController;
