d1e3cac756bdc37505949a52294807f63cf6eb8d
Naomi Himley <naomihimley@gmail.com>
2014-06-19 12:00:32 -0500

    added changes to specify which region didDetermineState is being called for

--- PubChatter/ProfileViewController.m
+++ PubChatter/ProfileViewController.m
@@ -17,7 +17,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *sexualOrientationLabel;
 @property (weak, nonatomic) IBOutlet UILabel *favDrinkLabel;
 
-@property (strong, nonatomic) CLBeaconRegion *beaconRegion;
+@property (strong, nonatomic) CLBeaconRegion *richRegion;
 @property (strong, nonatomic) CLBeaconRegion *estimoteRegion;
 @property (strong, nonatomic) CLLocationManager *locationManager;
 @property BOOL inARegion;
@@ -45,6 +45,7 @@
         UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Unable To Monitor Location" message:@"Only works on iOS 5 and later" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
         [alert show];
     }
+    self.navigationItem.title= @"PubChatter";
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -68,7 +69,7 @@
         {
             [self createBeaconRegion];
             [self.locationManager startUpdatingLocation];
-            [self.locationManager requestStateForRegion:self.beaconRegion];
+            [self.locationManager requestStateForRegion:self.richRegion];
             [self.locationManager requestStateForRegion:self.estimoteRegion];
         }
     }
@@ -139,20 +140,19 @@
 {
     //all estimote iBeacons
     NSUUID *estimoteUUID = [[NSUUID alloc] initWithUUIDString:@"B9407F30-F5F8-466E-AFF9-25556B57FE6D"];
-    self.estimoteRegion = [[CLBeaconRegion alloc]initWithProximityUUID:estimoteUUID identifier:@"irrelevant"];
+    self.estimoteRegion = [[CLBeaconRegion alloc]initWithProximityUUID:estimoteUUID identifier:@"anyEstimoteBeacon"];
     self.estimoteRegion.notifyOnEntry = YES;
     self.estimoteRegion.notifyOnExit = YES;
-    //location manager sends beacon notifications when the user turns on the display and the device is already inside the region. These notifications are sent even if your app is not running.
     self.estimoteRegion.notifyEntryStateOnDisplay = YES;
     [self.locationManager startMonitoringForRegion:self.estimoteRegion];
 
     //rich's phone
     NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"8492E75F-4FD6-469D-B132-043FE94921D8"];
-    self.beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid identifier:@"irrelevant identifier"];
-    self.beaconRegion.notifyOnEntry = YES;
-    self.beaconRegion.notifyOnExit=YES;
-    self.beaconRegion.notifyEntryStateOnDisplay=YES;
-    [self.locationManager startMonitoringForRegion:self.beaconRegion];
+    self.richRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid identifier:@"richiPhone"];
+    self.richRegion.notifyOnEntry = YES;
+    self.richRegion.notifyOnExit=YES;
+    self.richRegion.notifyEntryStateOnDisplay=YES;
+    [self.locationManager startMonitoringForRegion:self.richRegion];
 }
 
 #pragma mark - Parse Login Methods
@@ -172,9 +172,9 @@
     [self.tabBarController setSelectedIndex:0];
     if ([CLLocationManager isMonitoringAvailableForClass:[CLBeaconRegion class]])
     {
-        [self.locationManager stopMonitoringForRegion:self.beaconRegion];
+        [self.locationManager stopMonitoringForRegion:self.richRegion];
         [self.locationManager stopMonitoringForRegion:self.estimoteRegion];
-        [self.locationManager stopRangingBeaconsInRegion:self.beaconRegion];
+        [self.locationManager stopRangingBeaconsInRegion:self.richRegion];
         [self.locationManager stopRangingBeaconsInRegion:self.estimoteRegion];
     }
 }
@@ -185,22 +185,37 @@
     if (state == CLRegionStateInside)
     {
         //this method is for when the user opens the app already inside of a region. DidEnterRegion will not get called because they wont cross the boundary, but this checks the CLRegionState and changes our bool.
-        [manager startRangingBeaconsInRegion:self.beaconRegion];
+        [manager startRangingBeaconsInRegion:self.richRegion];
         [manager startRangingBeaconsInRegion:self.estimoteRegion];
-        NSLog(@"CLRegionStateInside");
         self.inARegion = YES;
     }
     else if (state == CLRegionStateOutside)
     {
-        NSLog(@"CLRegionStateOutside");
+        if ([region.identifier isEqualToString:@"richiPhone"])
+        {
+            NSLog(@"CLRegionStateOutside of rich's phone");
+            PFQuery *queryForBar = [PFQuery queryWithClassName:@"Bar"];
+            [queryForBar whereKey:@"objectId" equalTo:@"UL0yMO2bGj"];
+            [queryForBar findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+                PFObject *bar = [objects firstObject];
+                [bar removeObject:[PFUser currentUser] forKey:@"usersInBar"];
+                [bar saveInBackground];
+            }];
+        }
+        if ([region.identifier isEqualToString:@"anyEstimoteBeacon"])
+        {
+            NSLog(@"CLRegionStateOutside of estimote");
+            PFQuery *queryForBar = [PFQuery queryWithClassName:@"Bar"];
+            [queryForBar whereKey:@"objectId" equalTo:@"UL0yMO2bGj"];
+            [queryForBar findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+                PFObject *bar = [objects firstObject];
+                [bar removeObject:[PFUser currentUser] forKey:@"usersInBar"];
+                [bar saveInBackground];
+            }];
+
+        }
         //checks parse to see what bar the user was in and removes them.
-        PFQuery *queryForBar = [PFQuery queryWithClassName:@"Bar"];
-        [queryForBar whereKey:@"usersInBar" equalTo:[PFUser currentUser]];
-        [queryForBar findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-            PFObject *bar = [objects firstObject];
-            [bar removeObject:[PFUser currentUser] forKey:@"usersInBar"];
-            [bar saveInBackground];
-        }];
+
         self.inARegion = NO;
         self.navigationItem.title= @"PubChatter";
     }
@@ -226,12 +241,16 @@
             [queryForBar findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
                 PFObject *bar = [objects firstObject];
                 NSArray *arrayOfUsers = [NSArray arrayWithArray:[bar objectForKey:@"usersInBar"]];
-                if (![arrayOfUsers containsObject:[PFUser currentUser]]) {
-                    [bar addObject:[PFUser currentUser] forKey:@"usersInBar"];
-                    [bar saveInBackground];
-                    NSLog(@"addingObject");
-                    self.inARegion = NO;
+                NSLog(@"arrayOfUsers: %@", arrayOfUsers);
+                for (PFUser *user in arrayOfUsers) {
+                    if (![[user objectForKey:@"username"] isEqual:[[PFUser currentUser]objectForKey:@"username"]]) {
+                        [bar addObject:[PFUser currentUser] forKey:@"usersInBar"];
+                        [bar saveInBackground];
+                        NSLog(@"addingObject to old town");
+                        self.inARegion = NO;
+                    }
                 }
+
                 self.inARegion = NO;
                 self.navigationItem.title = [bar objectForKey:@"barName"];
             }];
@@ -244,10 +263,10 @@
             [queryForBar findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
                 PFObject *bar = [objects firstObject];
                 NSArray *arrayOfUsers = [NSArray arrayWithArray:[bar objectForKey:@"usersInBar"]];
-                if (![arrayOfUsers containsObject:[PFUser currentUser]]) {
+                if (![arrayOfUsers containsObject:[[PFUser currentUser] objectId]]) {
                     [bar addObject:[PFUser currentUser] forKey:@"usersInBar"];
                     [bar saveInBackground];
-                    NSLog(@"addingObject");
+                    NSLog(@"addingObject to rich's iphone");
                     self.inARegion = NO;
                 }
                 self.inARegion = NO;
@@ -259,8 +278,14 @@
 
 - (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region
 {
-    [self.locationManager startRangingBeaconsInRegion:self.beaconRegion];
-    [self.locationManager startRangingBeaconsInRegion:self.estimoteRegion];
+    if ([region.identifier isEqualToString:@"anyEstimoteBeacon"])
+    {
+        [self.locationManager startRangingBeaconsInRegion:self.estimoteRegion];
+    }
+    if ([region.identifier isEqualToString:@"richiPhone"])
+    {
+        [self.locationManager startRangingBeaconsInRegion:self.richRegion];
+    }
     self.inARegion = YES;
     NSLog(@"did enter");
 }
@@ -268,7 +293,7 @@
 - (void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region
 {
     NSLog(@"did exit");
-    [self.locationManager stopRangingBeaconsInRegion:self.beaconRegion];
+    [self.locationManager stopRangingBeaconsInRegion:self.richRegion];
     [self.locationManager stopMonitoringForRegion:self.estimoteRegion];
     self.inARegion = NO;
     PFQuery *queryForBar = [PFQuery queryWithClassName:@"Bar"];
