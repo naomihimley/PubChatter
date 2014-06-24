299564012dd7fdaf6db6c056a8e2d4d9a8ffc403
Rich Fellure <rfellure24@gmail.com>
2014-06-23 19:56:57 -0500

    Left Slide Drawer may be working

---
+++ LeftSlideOutViewController.h
@@ -0,0 +1,15 @@
+//
+//  LeftSlideOutViewController.h
+//  PubChatter
+//
+//  Created by Richard Fellure on 6/23/14.
+//  Copyright (c) 2014 Naomi Himley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeftSlideOutViewController : UITableViewController
+
+@property NSArray *users;
+
+@end
