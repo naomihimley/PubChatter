af7922597fb1582d08c4eaf62b5ce1aa1a2982ed
Rich Fellure <rfellure24@gmail.com>
2014-06-24 23:39:03 -0500

    Changed the flow

--- LeftSideSlideOutTableViewController.m
+++ LeftSideSlideOutTableViewController.m
@@ -10,12 +10,15 @@
 #import "ChatViewController.h"
 #import "ChatBoxViewController.h"
 #import "AppDelegate.h"
+#import "ListOfUsersTableViewCell.h"
+#import "OPPViewController.h"
 #import <Parse/Parse.h>
 
 @interface LeftSideSlideOutTableViewController ()
 
 @property AppDelegate *appDelegate;
-@property NSArray *users;
+@property NSMutableArray *cellArray;
+@property NSMutableArray *users;
 
 @end
 
@@ -25,11 +28,24 @@
 {
     [super viewDidLoad];
     self.appDelegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
-    self.users = [NSArray array];
+    self.users = [NSMutableArray array];
+    self.cellArray = [NSMutableArray array];
     [[NSNotificationCenter defaultCenter]addObserver:self
                                             selector:@selector(queryForUsers)
                                                 name:@"MCDidChangeStateNotification"
                                               object:nil];
+    if ([PFUser currentUser])
+    {
+        [self.appDelegate.mcManager setupPeerAndSessionWithDisplayName:[[PFUser currentUser]objectForKey:@"username"]];
+        [self.appDelegate.mcManager advertiseSelf:YES];
+        NSLog(@"username %@", [[PFUser currentUser]objectForKey:@"username"]);
+    }
+
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(peerDidChangeStateWithNotification:) name:@"MCDidChangeStateNotification" object:nil];
+
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(queryForUsers) name:@"MCFoundAdvertisingPeer" object:nil];
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(peerStoppedAdvertising:) name:@"MCPeerStopAdvertising" object:nil];
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(receivedInvitationForConnection:) name:@"MCReceivedInvitation" object:nil];
 }
 
 -(void)viewWillAppear:(BOOL)animated
@@ -47,13 +63,46 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    ListOfUsersTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
     NSDictionary *dictionary = [self.users objectAtIndex:indexPath.row];
 
     PFUser *user = [dictionary objectForKey:@"user"];
 
-    cell.textLabel.text = [user objectForKey:@"username"];
+    cell.userNameLabel.text = [user objectForKey:@"username"];
+    cell.chatButton.tag = indexPath.row;
+    [self.cellArray addObject:cell];
+    cell.tag = [self.users indexOfObject:dictionary];
 
+    if ([user objectForKey:@"age"]) {
+        cell.userAgeLabel.text = [NSString stringWithFormat:@"%@",[user objectForKey:@"age"]];
+    }
+    else
+    {
+        cell.userAgeLabel.text = @"";
+    }
+
+    if ([user [@"gender"] isEqual:@0])
+    {
+        cell.genderLabel.text = @"F";
+    }
+    else if ([user[@"gender"] isEqual:@1])
+    {
+        cell.genderLabel.text = @"M";
+    }
+    else if ([user [@"gender"] isEqual:@2])
+    {
+        cell.genderLabel.text = @"Other";
+        [cell.genderLabel sizeToFit];
+    }
+    else
+    {
+        cell.genderLabel.text = @"";
+    }
+
+    PFFile *imageFile = [user objectForKey:@"picture"];
+    [imageFile getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+        cell.userImage.image = [UIImage imageWithData:data];
+    }];
     return cell;
 }
 
@@ -61,15 +110,15 @@
 
 -(void)queryForUsers
 {
-    NSMutableArray *array = [NSMutableArray array];
-    PFQuery *query = [PFQuery queryWithClassName:@"_User"];
+    [self.users removeAllObjects];
 
+    PFQuery *query = [PFQuery queryWithClassName:@"_User"];
     [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error)
      {
          if (!error)
          {
              NSArray *users = [[NSArray alloc]initWithArray:objects];
-             for (MCPeerID *peerID in self.appDelegate.mcManager.session.connectedPeers)
+             for (MCPeerID *peerID in self.appDelegate.mcManager.advertisingUsers)
              {
                  for (PFUser *user in users)
                  {
@@ -77,27 +126,119 @@
                      {
                          NSDictionary *dictionary = @{@"peerID": peerID,
                                                       @"user": user};
-                         if (self.users.count <= self.appDelegate.mcManager.session.connectedPeers.count)
+                         if (self.users.count <= self.appDelegate.mcManager.advertisingUsers.count)
                          {
-                             [array addObject:dictionary];
+                             [self.users addObject:dictionary];
                          }
                      }
                  }
-                 self.users = [NSArray arrayWithArray:array];
                  [self.tableView reloadData];
              }
          }
      }];
 }
 
--(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+#pragma mark - Action for Button sending invitation
+
+- (IBAction)onButtonTappedSendInvitation:(id)sender
 {
-    ChatBoxViewController *chatBoxVC = segue.destinationViewController;
-    NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+    UIButton *button = (UIButton *)sender;
+
+    UITableViewCell *cell = (UITableViewCell *)[[[sender superview]superview]superview];
+    NSIndexPath *indexPath = [self.tableView indexPathForCell:cell];
 
     NSDictionary *dictionary = [self.users objectAtIndex:indexPath.row];
 
-    chatBoxVC.userDictionary = dictionary;
+    NSLog(@"user to send data too %@", dictionary);
+    MCPeerID *peerID = [dictionary objectForKey:@"peerID"];
+
+    [self.appDelegate.mcManager.browser invitePeer:peerID toSession:self.appDelegate.mcManager.session withContext:nil timeout:30];
+
+    if ([button.titleLabel.text isEqual:@"Chat"])
+    {
+        ChatBoxViewController *chatBoxVC = [[ChatBoxViewController alloc]init];
+        chatBoxVC.userDictionary = dictionary;
+
+        [self presentViewController:chatBoxVC animated:YES completion:nil];
+    }
+}
+
+#pragma mark - Private method for handling the changing of peer's state
+
+-(void)peerDidChangeStateWithNotification:(NSNotification *)notification
+{
+
+    MCPeerID *peerID = [[notification userInfo]objectForKey:@"peerID"];
+    NSLog(@"Changing state with notification 1");
+
+    for (NSDictionary *dictionary in self.users)
+    {
+        if ([[dictionary objectForKey:@"peerID"] isEqual:peerID])
+        {
+            int index = [self.users indexOfObject:dictionary];
+
+            for (ListOfUsersTableViewCell *userCell in self.cellArray)
+            {
+                if (userCell.tag == index)
+                {
+                    if ([[[notification userInfo]objectForKey:@"state"]intValue] == MCSessionStateConnected)
+                    {
+                        [userCell.chatButton setHighlighted:YES];
+                    }
+                }
+            }
+            
+        }
+    }
+}
+
+# pragma mark - Stopped Advertising method catcher
+
+-(void)peerStoppedAdvertising:(NSNotification *)notificaion
+{
+    MCPeerID *peerID = [[notificaion userInfo]objectForKey:@"peerID"];
+    NSDictionary *userDictionary = [NSDictionary new];
+
+    for (NSDictionary *dictionary in self.users)
+    {
+        if ([dictionary objectForKey:@"peerID"] == peerID)
+        {
+            userDictionary = dictionary;
+        }
+    }
+    [self.users removeObject:userDictionary];
+    NSLog(@"need to remove %@", userDictionary);
+    [self.tableView reloadData];
+}
+
+#pragma mark - Private method for handling receiving an invitation
+
+-(void)receivedInvitationForConnection:(NSNotification *)notification
+{
+    MCPeerID *peerID = [[notification userInfo]objectForKey:@"peerID"];
+    NSLog(@"peerID.displayName of sender %@", peerID.displayName);
+    NSString *alertViewTitle = [NSString stringWithFormat:@"%@ wants to connect and chat with you", peerID.displayName];
+    UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:alertViewTitle message:nil delegate:self cancelButtonTitle:@"Decline" otherButtonTitles:@"Accept", nil];
+    [alertView show];
+}
+
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    BOOL accept = (buttonIndex != alertView.cancelButtonIndex);
+
+    void (^invitationHandler)(BOOL, MCSession *) = [self.appDelegate.mcManager.invitationHandlerArray objectAtIndex:0];
+    invitationHandler(accept, self.appDelegate.mcManager.session);
+}
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    OPPViewController *oppVC = segue.destinationViewController;
+    NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+
+    NSDictionary *dictionary = [self.users objectAtIndex:indexPath.row];
+    PFUser *selectedUser = [dictionary objectForKey:@"user"];
+
+    oppVC.user = selectedUser;
 }
 
 @end
