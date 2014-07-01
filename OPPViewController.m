f94be84ab6b6b21c786e2194551267fbd7e67add
drw86312 <david.warner4@gmail.com>
2014-06-30 22:21:19 -0500

    Got scrolling in profile view

--- OPPViewController.m
+++ OPPViewController.m
@@ -11,15 +11,26 @@
 #import "ChatBoxViewController.h"
 #import "UIColor+DesignColors.h"
 
-@interface OPPViewController ()<UIAlertViewDelegate>
-@property (weak, nonatomic) IBOutlet UIImageView *imageView;
-@property (weak, nonatomic) IBOutlet UILabel *userNameLabel;
-@property (weak, nonatomic) IBOutlet UILabel *sexLabel;
-@property (weak, nonatomic) IBOutlet UITextView *bioLabel;
-@property (weak, nonatomic) IBOutlet UILabel *sexualOrientationLabel;
-@property (weak, nonatomic) IBOutlet UILabel *favDrinkLabel;
+@interface OPPViewController ()<UIAlertViewDelegate, UIScrollViewDelegate>
+
+@property (strong, nonatomic) UIImage *profileImage;
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *gender;
+@property (strong, nonatomic) NSString *bioText;
+@property (strong, nonatomic) NSString *age;
+@property (strong, nonatomic) NSString *sexualOrientation;
+@property (strong, nonatomic) NSString *favDrink;
+
+@property (strong, nonatomic) UIView *viewInBackground;
+@property (strong, nonatomic) UITextView *bioTextView;
+@property (strong, nonatomic) UILabel *nameageLabel;
+@property (strong, nonatomic) UILabel *genderLabel;
+@property (strong, nonatomic) UILabel *ageLabel;
+@property (strong, nonatomic) UILabel *interestedLabel;
+@property (strong, nonatomic) UILabel *favDrinkLabel;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
 @property (weak, nonatomic) IBOutlet UINavigationBar *navBar;
-@property (weak, nonatomic) IBOutlet UIView *backgroundLayoutView;
 @property AppDelegate *appDelegate;
 
 -(void)receivedInvitationForConnection: (NSNotification *)notification;
@@ -34,105 +45,179 @@
     [super viewDidLoad];
     self.chatArray = [NSMutableArray array];
     self.chatDictionaryArray = [NSMutableArray array];
-
-    self.userNameLabel.textColor =[UIColor nameColor];
-    self.navBar.backgroundColor = [UIColor navBarColor];
-    self.backgroundLayoutView.backgroundColor = [[UIColor backgroundColor]colorWithAlphaComponent:0.95f];
-    self.sexualOrientationLabel.textColor = [UIColor whiteColor];
-    self.sexLabel.textColor = [UIColor whiteColor];
-    self.bioLabel.editable = YES;
-    self.bioLabel.textColor = [UIColor whiteColor];
-    self.bioLabel.editable = NO;
-    self.favDrinkLabel.textColor = [UIColor whiteColor];
-    self.bioLabel.backgroundColor = [UIColor clearColor];
-    self.view.backgroundColor = [UIColor blackColor];
-
-    // Set name and age label.
-    self.userNameLabel.text = [self.user objectForKey:@"name"];
-
-    // Set gender label.
-    if ([self.user [@"gender"] isEqual:@0] && [self.user objectForKey:@"age"])
-    {
-        self.sexLabel.text = [NSString stringWithFormat:@"%@, female", [self.user objectForKey:@"age"]];
-        [self.sexLabel sizeToFit];
-    }
-    else if ([self.user[@"gender"] isEqual:@1] && [self.user objectForKey:@"age"])
-    {
-       self.sexLabel.text = [NSString stringWithFormat:@"%@, male", [self.user objectForKey:@"age"]];
-        [self.sexLabel sizeToFit];
-    }
-    else if ([self.user [@"gender"] isEqual:@2] && [self.user objectForKey:@"age"])
-    {
-       self.sexLabel.text = [NSString stringWithFormat:@"%@, other", [self.user objectForKey:@"age"]];
-        [self.sexLabel sizeToFit];
-    }
-    else if ([self.user [@"gender"] isEqual:@0])
-    {
-        self.sexLabel.text = @"male";
-    }
-    else if ([self.user [@"gender"] isEqual:@1])
-    {
-         self.sexLabel.text = @"female";
-    }
-    else if ([self.user [@"gender"] isEqual:@2])
-    {
-         self.sexLabel.text = @"other";
-    }
-    else if ([self.user objectForKey:@"age"])
-    {
-        self.sexLabel.text = [NSString stringWithFormat:@"%@",[self.user objectForKey:@"age"]];
-    }
-    else
-    {
-        self.sexLabel.text = @"No Info";
-    }
-
-    // Set about me.
-    if ([self.user objectForKey:@"bio"])
-    {
-        UIColor *color = [UIColor whiteColor];
-        NSString *name = [self.user objectForKey:@"name"];
-        UIFont *boldFont = [UIFont boldSystemFontOfSize:12.0];
-        NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"About %@\n%@", name, [self.user objectForKey:@"bio"]]];
-        [attrString addAttribute: NSFontAttributeName value: boldFont range: NSMakeRange(0, 6 + name.length)];
-        [attrString addAttribute:NSForegroundColorAttributeName value:color range:NSMakeRange(0, attrString.length)];
-        self.bioLabel.attributedText = attrString;
-    }
-    else
-    {
-        self.bioLabel.text = @"No Bio Info";
-    }
-
-    // Set sexual orientation label.
-    if ([self.user [@"sexualOrientation"] isEqual:@0])
-    {
-        self.sexualOrientationLabel.text = @"Interested in: Men";
-        [self.sexualOrientationLabel sizeToFit];
-    }
-    else if ([self.user[@"sexualOrientation"] isEqual:@1])
-    {
-        self.sexualOrientationLabel.text = @"Interested in: Women";
-        [self.sexualOrientationLabel sizeToFit];
-    }
-    else if ([self.user [@"sexualOrientation"] isEqual:@2])
-    {
-        self.sexualOrientationLabel.text = @"Bisexual";
-        [self.sexualOrientationLabel sizeToFit];
-    }
-    else
-    {
-        self.sexualOrientationLabel.text = @"No Info";
-    }
-
-    // Set profile image.
-    PFFile *file = [self.user objectForKey:@"picture"];
-    [file getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
-        self.imageView.image = [UIImage imageWithData:data];
-    }];
-
+    
     self.appDelegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
 
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(receivedInvitationForConnection:) name:@"MCReceivedInvitation" object:nil];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self getPersonalData];
+}
+
+
+-(void)addViewsToScrollView {
+
+    CGFloat verticalOffset = 10.0;
+
+    //Add imageview
+    UIImageView *profileImageView = [[UIImageView alloc] init];
+    profileImageView.frame = CGRectMake((self.scrollView.frame.size.width/2) -75, verticalOffset, 150, 150);
+    profileImageView.image = self.profileImage;
+    [self.scrollView addSubview:profileImageView];
+    verticalOffset = verticalOffset + profileImageView.frame.size.height + 10;
+
+    //Add name label
+    self.nameageLabel = [[UILabel alloc] init];
+    self.nameageLabel.frame = CGRectMake((self.scrollView.frame.size.width /2) - 140, verticalOffset, 280, 30);
+    self.nameageLabel.text = [NSString stringWithFormat:@"%@, %@", self.name, self.age];
+    self.nameageLabel.textAlignment = NSTextAlignmentCenter;
+    self.nameageLabel.textColor = [UIColor nameColor];
+    [self.nameageLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:17]];
+    [self.scrollView addSubview:self.nameageLabel];
+    verticalOffset = verticalOffset + self.nameageLabel.frame.size.height + 10;
+
+    //Add gender label
+    self.genderLabel = [[UILabel alloc] init];
+    self.genderLabel.frame = CGRectMake((self.scrollView.frame.size.width /2) - 140, verticalOffset, 280, 30);
+    self.genderLabel.text = self.gender;
+    self.genderLabel.textAlignment = NSTextAlignmentCenter;
+    self.genderLabel.textColor = [UIColor whiteColor];
+    [self.genderLabel setFont:[UIFont systemFontOfSize:17.0]];
+    [self.scrollView addSubview:self.genderLabel];
+    verticalOffset = verticalOffset + self.genderLabel.frame.size.height + 10;
+
+    //Add about me label
+    UILabel *aboutMeLabel = [[UILabel alloc] init];
+    aboutMeLabel.frame = CGRectMake((self.scrollView.frame.size.width /2) - 140, verticalOffset, 280, 30);
+    aboutMeLabel.text = [NSString stringWithFormat:@"About %@", self.name];
+    aboutMeLabel.textAlignment = NSTextAlignmentCenter;
+    aboutMeLabel.textColor = [UIColor whiteColor];
+    [self.scrollView addSubview:aboutMeLabel];
+    verticalOffset = verticalOffset + aboutMeLabel.frame.size.height;
+
+    //Add bio textView
+    self.bioTextView = [[UITextView alloc] init];
+    self.bioTextView.frame = CGRectMake((self.scrollView.frame.size.width /2) - 140, verticalOffset, 280, 70);
+    self.bioTextView.text = self.bioText;
+    self.bioTextView.editable = NO;
+    self.bioTextView.textAlignment = NSTextAlignmentCenter;
+    self.bioTextView.textColor = [UIColor whiteColor];
+    self.bioTextView.backgroundColor = [UIColor clearColor];
+    [self.scrollView addSubview:self.bioTextView];
+    verticalOffset = verticalOffset + self.bioTextView.frame.size.height + 10;
+
+    //Add interested label
+    self.interestedLabel = [[UILabel alloc] init];
+    self.interestedLabel.frame = CGRectMake((self.scrollView.frame.size.width /2) - 140, verticalOffset, 280, 30);
+    self.interestedLabel.text = self.sexualOrientation;
+    self.interestedLabel.textAlignment = NSTextAlignmentCenter;
+    self.interestedLabel.textColor = [UIColor whiteColor];
+    [self.scrollView addSubview:self.interestedLabel];
+    verticalOffset = verticalOffset + self.interestedLabel.frame.size.height + 10;
+
+    //Add Favorite drink label
+    self.favDrinkLabel = [[UILabel alloc] init];
+    self.favDrinkLabel.frame = CGRectMake((self.scrollView.frame.size.width /2) - 140, verticalOffset, 280, 30);
+    self.favDrinkLabel.text = self.favDrink;
+    self.favDrinkLabel.textAlignment = NSTextAlignmentCenter;
+    self.favDrinkLabel.textColor = [UIColor whiteColor];
+    [self.scrollView addSubview:self.favDrinkLabel];
+    verticalOffset = verticalOffset + self.favDrinkLabel.frame.size.height + 15;
+
+    self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.size.width, verticalOffset);
+
+    //Add background view
+    UIView *backgroundView = [[UIView alloc] init];
+    backgroundView.frame = CGRectMake(0, 0, self.scrollView.frame.size.width, verticalOffset);
+    backgroundView.backgroundColor = [[UIColor backgroundColor]colorWithAlphaComponent:0.95f];
+
+    [self.scrollView insertSubview:backgroundView atIndex:0];
+
+    self.scrollView.contentMode = UIViewContentModeScaleAspectFit;
+    self.scrollView.delegate = self;
+    
+    [self setStyle];
+}
+
+-(void)getPersonalData
+{
+
+    //Get name text.
+    self.name = [self.user objectForKey:@"name"];
+
+    //Get bio text.
+    if ([self.user objectForKey:@"bio"]) {
+
+        self.bioText = [self.user objectForKey:@"bio"];
+    }
+    else
+    {
+        self.bioText = @"";
+    }
+
+    //Get favorite drink.
+    if ([self.user objectForKey:@"favoriteDrink"])
+    {
+        self.favDrink = [self.user objectForKey:@"favoriteDrink"];
+    }
+
+    //Get gender.
+    if ([[self.user objectForKey:@"gender"] isEqual:@0])
+    {
+        self.gender = @"Female";
+    }
+    else if ([[[PFUser currentUser]objectForKey:@"gender"] isEqual:@1])
+    {
+        self.gender = @"Male";
+    }
+    else if ([[[PFUser currentUser]objectForKey:@"gender"] isEqual:@2])
+    {
+        self.gender = @"Other";
+    }
+
+    //Get age.
+    if ([self.user objectForKey:@"age"])
+    {
+        NSNumber *age = [self.user objectForKey:@"age"];
+        self.age = [NSString stringWithFormat:@"%@", age];
+        NSLog(@"%@", self.age);
+        NSLog(@"%@", [self.age class]);
+    }
+    else
+    {
+        self.age = @"";
+        NSLog(@"%@", self.age);
+        NSLog(@"%@", [self.age class]);
+    }
+
+    //Get sexual orientation.
+    if ([[self.user objectForKey:@"sexualOrientation"] isEqual:@0])
+    {
+        self.sexualOrientation = @"Interested in: Men";
+    }
+    else if ([[self.user objectForKey:@"sexualOrientation"] isEqual:@1])
+    {
+        self.sexualOrientation = @"Interested in: Women";
+    }
+    else if ([[self.user objectForKey:@"sexualOrientation"] isEqual:@2])
+    {
+        self.sexualOrientation = @"Bisexual";
+    }
+    else
+    {
+        self.sexualOrientation = @"";
+    }
+
+    //Get user image.
+    PFFile *file = [self.user objectForKey:@"picture"];
+    [file getDataInBackgroundWithBlock:^(NSData *data, NSError *error)
+     {
+         self.profileImage = [UIImage imageWithData:data];
+         [self addViewsToScrollView];
+     }];
 }
 
 #pragma mark - Alert Showing Invitation to join Session
@@ -156,6 +241,28 @@
 - (IBAction)onButtonPressedDismissVC:(id)sender
 {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - Styling method
+-(void)setStyle
+{
+    //Style nameagelabel
+    self.navigationItem.title= @"PubChat";
+    self.navigationController.navigationBar.backgroundColor = [UIColor navBarColor];
+    //    self.viewInBackground.opaque = YES;
+    //    self.viewInBackground.layer.opacity = 0.9f;
+    //    self.viewInBackground.alpha = 0.9f;
+    //    [self.viewInBackground setBackgroundColor:[[UIColor backgroundColor]colorWithAlphaComponent:0.9f]];
+    //
+    //    self.view.backgroundColor = [UIColor blackColor];
+    //    self.nameageLabel.textColor = [UIColor nameColor];
+    //    self.genderLabel.textColor = [UIColor whiteColor];
+    //    self.bioTextView.editable = YES;
+    //    self.bioTextView.textColor = [UIColor whiteColor];
+    //    self.bioTextView.editable = NO;
+    //    self.interestedLabel.textColor = [UIColor whiteColor];
+    //    self.favDrinkLabel.textColor = [UIColor whiteColor];
+    //    self.bioTextView.backgroundColor = [UIColor clearColor];
 }
 
 
--- PubChatter/ProfileViewController.m
+++ PubChatter/ProfileViewController.m
@@ -11,16 +11,24 @@
 #import "UIColor+DesignColors.h"
 
 @interface ProfileViewController ()<CLLocationManagerDelegate, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate, UIScrollViewDelegate>
-@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
-@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
-@property (weak, nonatomic) IBOutlet UILabel *genderLabel;
-@property (weak, nonatomic) IBOutlet UITextView *bioTextView;
-@property (weak, nonatomic) IBOutlet UILabel *sexualOrientationLabel;
-@property (weak, nonatomic) IBOutlet UILabel *favDrinkLabel;
-@property (weak, nonatomic) IBOutlet UIView *viewInBackground;
+@property (strong, nonatomic) UIImage *profileImage;
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *gender;
+@property (strong, nonatomic) NSString *bioText;
+@property (strong, nonatomic) NSString *age;
+@property (strong, nonatomic) NSString *sexualOrientation;
+@property (strong, nonatomic) NSString *favDrink;
+
+@property (strong, nonatomic) UIView *viewInBackground;
+@property (strong, nonatomic) UITextView *bioTextView;
+@property (strong, nonatomic) UILabel *nameageLabel;
+@property (strong, nonatomic) UILabel *genderLabel;
+@property (strong, nonatomic) UILabel *ageLabel;
+@property (strong, nonatomic) UILabel *interestedLabel;
+@property (strong, nonatomic) UILabel *favDrinkLabel;
+
 @property AppDelegate *appDelegate;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-@property (weak, nonatomic) IBOutlet UIView *infoContainerView;
 
 -(void)didreceiveNotification:(NSNotification *)notification;
 
@@ -34,9 +42,90 @@
                                              selector:@selector(didreceiveNotification:)
                                                  name:@"userEnteredBar"
                                                object:nil];
-    [self setStyle];
+}
 
+-(void)addViewsToScrollView {
+
+    CGFloat verticalOffset = 10.0;
+
+    //Add imageview
+    UIImageView *profileImageView = [[UIImageView alloc] init];
+    profileImageView.frame = CGRectMake((self.scrollView.frame.size.width/2) -75, verticalOffset, 150, 150);
+    profileImageView.image = self.profileImage;
+    [self.scrollView addSubview:profileImageView];
+    verticalOffset = verticalOffset + profileImageView.frame.size.height + 10;
+
+    //Add name label
+    self.nameageLabel = [[UILabel alloc] init];
+    self.nameageLabel.frame = CGRectMake((self.scrollView.frame.size.width /2) - 140, verticalOffset, 280, 30);
+    self.nameageLabel.text = [NSString stringWithFormat:@"%@, %@", self.name, self.age];
+    self.nameageLabel.textAlignment = NSTextAlignmentCenter;
+    self.nameageLabel.textColor = [UIColor nameColor];
+    [self.nameageLabel setFont:[UIFont fontWithName:@"HelveticaNeue" size:17]];
+    [self.scrollView addSubview:self.nameageLabel];
+    verticalOffset = verticalOffset + self.nameageLabel.frame.size.height + 10;
+
+    //Add gender label
+    self.genderLabel = [[UILabel alloc] init];
+    self.genderLabel.frame = CGRectMake((self.scrollView.frame.size.width /2) - 140, verticalOffset, 280, 30);
+    self.genderLabel.text = self.gender;
+    self.genderLabel.textAlignment = NSTextAlignmentCenter;
+    self.genderLabel.textColor = [UIColor whiteColor];
+    [self.genderLabel setFont:[UIFont systemFontOfSize:17.0]];
+    [self.scrollView addSubview:self.genderLabel];
+    verticalOffset = verticalOffset + self.genderLabel.frame.size.height + 10;
+
+    //Add about me label
+    UILabel *aboutMeLabel = [[UILabel alloc] init];
+    aboutMeLabel.frame = CGRectMake((self.scrollView.frame.size.width /2) - 140, verticalOffset, 280, 30);
+    aboutMeLabel.text = [NSString stringWithFormat:@"About %@", self.name];
+    aboutMeLabel.textAlignment = NSTextAlignmentCenter;
+    aboutMeLabel.textColor = [UIColor whiteColor];
+    [self.scrollView addSubview:aboutMeLabel];
+    verticalOffset = verticalOffset + aboutMeLabel.frame.size.height;
+
+    //Add bio textView
+    self.bioTextView = [[UITextView alloc] init];
+    self.bioTextView.frame = CGRectMake((self.scrollView.frame.size.width /2) - 140, verticalOffset, 280, 70);
+    self.bioTextView.text = self.bioText;
+    self.bioTextView.editable = NO;
+    self.bioTextView.textAlignment = NSTextAlignmentCenter;
+    self.bioTextView.textColor = [UIColor whiteColor];
+    self.bioTextView.backgroundColor = [UIColor clearColor];
+    [self.scrollView addSubview:self.bioTextView];
+    verticalOffset = verticalOffset + self.bioTextView.frame.size.height + 10;
+
+    //Add interested label
+    self.interestedLabel = [[UILabel alloc] init];
+    self.interestedLabel.frame = CGRectMake((self.scrollView.frame.size.width /2) - 140, verticalOffset, 280, 30);
+    self.interestedLabel.text = self.sexualOrientation;
+    self.interestedLabel.textAlignment = NSTextAlignmentCenter;
+    self.interestedLabel.textColor = [UIColor whiteColor];
+    [self.scrollView addSubview:self.interestedLabel];
+    verticalOffset = verticalOffset + self.interestedLabel.frame.size.height + 10;
+
+    //Add Favorite drink label
+    self.favDrinkLabel = [[UILabel alloc] init];
+    self.favDrinkLabel.frame = CGRectMake((self.scrollView.frame.size.width /2) - 140, verticalOffset, 280, 30);
+    self.favDrinkLabel.text = self.favDrink;
+    self.favDrinkLabel.textAlignment = NSTextAlignmentCenter;
+    self.favDrinkLabel.textColor = [UIColor whiteColor];
+    [self.scrollView addSubview:self.favDrinkLabel];
+    verticalOffset = verticalOffset + self.favDrinkLabel.frame.size.height + 15;
+
+    self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.size.width, verticalOffset);
+
+    //Add background view
+    UIView *backgroundView = [[UIView alloc] init];
+    backgroundView.frame = CGRectMake(0, 0, self.scrollView.frame.size.width, verticalOffset);
+    backgroundView.backgroundColor = [[UIColor backgroundColor]colorWithAlphaComponent:0.95f];
+
+    [self.scrollView insertSubview:backgroundView atIndex:0];
+
+    self.scrollView.contentMode = UIViewContentModeScaleAspectFit;
     self.scrollView.delegate = self;
+
+    [self setStyle];
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -48,93 +137,80 @@
 
 -(void)getParseData
 {
-    PFFile *file = [[PFUser currentUser]objectForKey:@"picture"];
 
-    self.nameLabel.text = [[PFUser currentUser]objectForKey:@"name"];
+    //Get name text.
+    self.name = [[PFUser currentUser]objectForKey:@"name"];
 
-    [file getDataInBackgroundWithBlock:^(NSData *data, NSError *error)
-     {
-         self.profileImageView.image = [UIImage imageWithData:data];
-     }];
-
+    //Get bio text.
     if ([[PFUser currentUser]objectForKey:@"bio"]) {
 
-        UIColor *color = [UIColor whiteColor];
-        NSString *name = [[PFUser currentUser]objectForKey:@"name"];
-        UIFont *boldFont = [UIFont boldSystemFontOfSize:12.0];
-        NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"About %@\n%@", name, [[PFUser currentUser]objectForKey:@"bio"]]];
-        [attrString addAttribute: NSFontAttributeName value: boldFont range: NSMakeRange(0, 6 + name.length)];
-        [attrString addAttribute:NSForegroundColorAttributeName value:color range:NSMakeRange(0, attrString.length)];
-        self.bioTextView.attributedText = attrString;
-        self.bioTextView.textAlignment = NSTextAlignmentCenter;
+        self.bioText = [[PFUser currentUser]objectForKey:@"bio"];
     }
     else
     {
-        self.bioTextView.text = @"No Bio Info";
+        self.bioText = @"";
     }
 
+    //Get favorite drink.
     if ([[PFUser currentUser]objectForKey:@"favoriteDrink"])
     {
-        self.favDrinkLabel.text = [[PFUser currentUser]objectForKey:@"favoriteDrink"];
-        [self.favDrinkLabel sizeToFit];
+        self.favDrink = [[PFUser currentUser]objectForKey:@"favoriteDrink"];
     }
-     if ([[[PFUser currentUser]objectForKey:@"gender"] isEqual:@0] && [[PFUser currentUser]objectForKey:@"age"])
+
+    //Get gender.
+    if ([[[PFUser currentUser]objectForKey:@"gender"] isEqual:@0])
      {
-         self.genderLabel.text = [NSString stringWithFormat:@"%@, female", [[PFUser currentUser]objectForKey:@"age"]];
-         [self.genderLabel sizeToFit];
-     }
-     else if ([[[PFUser currentUser]objectForKey:@"gender"] isEqual:@1] && [[PFUser currentUser]objectForKey:@"age"])
-     {
-         self.genderLabel.text = [NSString stringWithFormat:@"%@, male", [[PFUser currentUser]objectForKey:@"age"]];
-         [self.genderLabel sizeToFit];
-     }
-     else if ([[[PFUser currentUser]objectForKey:@"gender"] isEqual:@2] && [[PFUser currentUser]objectForKey:@"age"])
-     {
-         self.genderLabel.text = [NSString stringWithFormat:@"%@, other", [[PFUser currentUser]objectForKey:@"age"]];
-         [self.genderLabel sizeToFit];
-     }
-     else if ([[[PFUser currentUser]objectForKey:@"gender"] isEqual:@0])
-     {
-         self.genderLabel.text = @"female";
-         [self.genderLabel sizeToFit];
+         self.gender = @"Female";
      }
      else if ([[[PFUser currentUser]objectForKey:@"gender"] isEqual:@1])
      {
-         self.genderLabel.text = @"male";
-         [self.genderLabel sizeToFit];
+         self.gender = @"Male";
      }
      else if ([[[PFUser currentUser]objectForKey:@"gender"] isEqual:@2])
      {
-         self.genderLabel.text = @"other";
-         [self.genderLabel sizeToFit];
+         self.gender = @"Other";
      }
-    else if ([[PFUser currentUser]objectForKey:@"age"])
+
+    //Get age.
+    if ([[PFUser currentUser]objectForKey:@"age"])
     {
-        self.genderLabel.text = [NSString stringWithFormat:@"%@", [[PFUser currentUser]objectForKey:@"age"]];
+        NSNumber *age = [[PFUser currentUser]objectForKey:@"age"];
+        self.age = [NSString stringWithFormat:@"%@", age];
+        NSLog(@"%@", self.age);
+        NSLog(@"%@", [self.age class]);
     }
      else
      {
-         self.genderLabel.text = @"No Info";
+         self.age = @"";
+         NSLog(@"%@", self.age);
+         NSLog(@"%@", [self.age class]);
      }
+
+    //Get sexual orientation.
      if ([[[PFUser currentUser]objectForKey:@"sexualOrientation"] isEqual:@0])
      {
-         self.sexualOrientationLabel.text = @"Interested in: Men";
-         [self.sexualOrientationLabel sizeToFit];
+         self.sexualOrientation = @"Interested in: Men";
      }
      else if ([[[PFUser currentUser]objectForKey:@"sexualOrientation"] isEqual:@1])
      {
-         self.sexualOrientationLabel.text = @"Interested in: Women";
-         [self.sexualOrientationLabel sizeToFit];
+         self.sexualOrientation = @"Interested in: Women";
      }
      else if ([[[PFUser currentUser]objectForKey:@"sexualOrientation"] isEqual:@2])
      {
-         self.sexualOrientationLabel.text = @"Bisexual";
-         [self.sexualOrientationLabel sizeToFit];
+         self.sexualOrientation = @"Bisexual";
      }
      else
      {
-         self.sexualOrientationLabel.text = @"";
+         self.sexualOrientation = @"";
      }
+
+    //Get user image.
+    PFFile *file = [[PFUser currentUser]objectForKey:@"picture"];
+    [file getDataInBackgroundWithBlock:^(NSData *data, NSError *error)
+     {
+         self.profileImage = [UIImage imageWithData:data];
+         [self addViewsToScrollView];
+     }];
 }
 
 #pragma mark - Segue Methods
@@ -159,21 +235,23 @@
 #pragma mark - Styling method
 -(void)setStyle
 {
+    //Style nameagelabel
     self.navigationItem.title= @"PubChat";
     self.navigationController.navigationBar.backgroundColor = [UIColor navBarColor];
-    self.viewInBackground.opaque = YES;
-    self.viewInBackground.layer.opacity = 0.9f;
-    self.viewInBackground.alpha = 0.9f;
-    [self.viewInBackground setBackgroundColor:[[UIColor backgroundColor]colorWithAlphaComponent:0.9f]];
-
-    self.view.backgroundColor = [UIColor blackColor];
-    self.nameLabel.textColor = [UIColor nameColor];
-    self.genderLabel.textColor = [UIColor whiteColor];
-    self.bioTextView.editable = YES;
-    self.bioTextView.textColor = [UIColor whiteColor];
-    self.bioTextView.editable = NO;
-    self.sexualOrientationLabel.textColor = [UIColor whiteColor];
-    self.favDrinkLabel.textColor = [UIColor whiteColor];
-    self.bioTextView.backgroundColor = [UIColor clearColor];
+//    self.viewInBackground.opaque = YES;
+//    self.viewInBackground.layer.opacity = 0.9f;
+//    self.viewInBackground.alpha = 0.9f;
+//    [self.viewInBackground setBackgroundColor:[[UIColor backgroundColor]colorWithAlphaComponent:0.9f]];
+//
+//    self.view.backgroundColor = [UIColor blackColor];
+//    self.nameageLabel.textColor = [UIColor nameColor];
+//    self.genderLabel.textColor = [UIColor whiteColor];
+//    self.bioTextView.editable = YES;
+//    self.bioTextView.textColor = [UIColor whiteColor];
+//    self.bioTextView.editable = NO;
+//    self.interestedLabel.textColor = [UIColor whiteColor];
+//    self.favDrinkLabel.textColor = [UIColor whiteColor];
+//    self.bioTextView.backgroundColor = [UIColor clearColor];
 }
+
 @end
