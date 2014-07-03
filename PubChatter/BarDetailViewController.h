b06975647e8249eca5d83ac5e88f9b6a5f057f84
Rich Fellure <rfellure24@gmail.com>
2014-07-02 19:17:10 -0500

    UI changes continued

---
+++ PubChatter/BarDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  BarDetailViewController.h
+//  PubChatter
+//
+//  Created by David Warner on 6/16/14.
+//  Copyright (c) 2014 Naomi Himley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "YelpBar.h"
+
+@interface BarDetailViewController : UIViewController
+
+@property YelpBar *barFromSourceVC;
+
+@end
